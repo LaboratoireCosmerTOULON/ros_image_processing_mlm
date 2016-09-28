0eb5a57ede2333fb1e1a4519f07f4bfd0af34fdb
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-09-28 15:57:57 +0200

    update comments in header file

--- include/ros_image_processing_mlm/colorDetection_mlm.hpp
+++ include/ros_image_processing_mlm/colorDetection_mlm.hpp
@@ -14,11 +14,12 @@ namespace ml
 {
 	/*!
 	* author : Matheus Laranjeira
-	* date   : 06/2016
+	* date   : 03/06/2016
 	* 
-	* \brief detects orange contours with canny edge detector and image color segmentation in HSV space
-	* \param contours, contour set of countour points grouped by connexity (see opencv findContour fonction) 
-	* \return bRect, a non oriented rectangle  
+	* \brief 	detects orange contours with canny edge detector and image color segmentation in HSV space
+	* \param	src		: source image
+	* 			imgMap	: BW image with detected orange spots
+	* \return 
 	*/
 	void cannyDetector(cv::Mat src, cv::Mat &imgMap);
 
@@ -27,9 +28,10 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 03/06/2016
 	* 
-	* \brief given an image, find pixels of specified color 
-	* \param  an opencv image
-	* \return the corresponding binary image 
+	* \brief 	given an image, find orange pixels
+	* \param  	imgSrc		: source image
+	* 			imgMapMorpho: BW image with orange pixels detected
+	* \return 
 	*/
 	void colorDetector(	const cv::Mat& imgSrc, cv::Mat& imgMapMorpho );
 
@@ -38,9 +40,14 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 06/06/2016
 	* 
-	* \brief create ROI where rope should be detected
-	* \param roi
-	* \return roi created. If roi.height = 0 or roi.width = 0, stop seeking for rope
+	* \brief 	create ROIs where rope should be detected
+	* \param 	linePoints		: vector containing the line segments coordinates, 
+	* 			lineAngles		: vector containing line segments angles wrt vertical,
+	* 			imgViz			: image for visualization,
+	* 			minLineLengthmin: minimum length of line (line segments shorter than this are rejected)
+	* 			maxLineGap		: maximum allowed gap between line segments to treat them as single line
+	* 			roi				: vector containing ROIs where rope should be detected
+	* \return 
 	*/
 	void createROI( std::vector<std::vector<int> > linePoints,
 					std::vector<float> lineAngles,
@@ -50,13 +57,14 @@ namespace ml
 					std::vector<cv::Rect> &roi );
 
 
+
 	/*!
 	* author : Matheus Laranjeira
 	* date   : 03/2016
 	* 
-	* \brief find the non oriented bounding boxe that fits with the larger area contour
-	* \param contours, contour set of countour points grouped by connexity (see opencv findContour fonction) 
-	* \return bRect, a non oriented rectangle  
+	* \brief	find the non oriented bounding boxe that fits with the larger area contour
+	* \param	contours	: contour set of countour points grouped by connexity (see opencv findContour fonction) 
+	* \return	bRect		: a non oriented rectangle  
 	*/
 	cv::Rect findBoundingBoxe(const std::vector<std::vector<cv::Point> > &);
 
@@ -64,9 +72,9 @@ namespace ml
 	/*!
 	* author : Claire Dune
 	* date   : 11/03/2016
-	* \brief find the oriented boxe that fits with the largest area contour
-	* \param a contour (see findContour)
-	* \return an oriented rectangle
+	* \brief	find the oriented boxe that fits with the largest area contour
+	* \param	contours	: (see cv::findContour)
+	* \return 	minRect		: an oriented rectangle
 	*/
 	cv::RotatedRect findOrientedBoxe(const std::vector<std::vector<cv::Point> > &);
 
@@ -75,8 +83,10 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 03/2016
 	* 
-	* \brief given
-	* \param  an opencv image
+	* \brief	finds contours of orange objects in the image
+	* \param  	imgSrc		: source image
+	* 			imgMapColor	: BW image with orange spots detected
+	* 			imgMapMorpho: imgMapColor after some morphological operations
 	* \return a set of closed contours  
 	*/
 	std::vector<std::vector<cv::Point> > findColorContour(	const cv::Mat&,
@@ -103,9 +113,13 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 03/2016
 	* 
-	* \brief detects an orange rope in a given image
-	* \param  an opencv image
-	* \return the angle alpha between the image vertical and the rope top part and the line bottom end
+	* \brief	detects an orange rope by dividing the source image in successive ROIs. Calculates 'angle' and 'lineEnd'
+	* \param 	imgSrc	: source image
+	* 			imgMap	: BW image with detected orange spots
+	* 			imgViz	: image for ROI visualization
+	* 			alpha	: line angle wrt vertical
+	* 			lineEnd	: image coordinates of line end
+	* \return
 	*/
 	void lineDetector(cv::Mat img, cv::Mat &imgMap, cv::Mat &imgViz, float &alpha, float &lineEnd);
 
@@ -114,9 +128,10 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 12/06/2016
 	* 
-	* \brief  sets line right direction: line begin is near to predecessor line end
-	* \param  vector of lines representing segmented rope
-	* \return the segment to be added to the vector of lines with right direction
+	* \brief  	sets line right direction: line begin is near to predecessor line end
+	* \param  	lineAngles	: vector containing the angles of the segments of line wrt vertical
+	* 			bline		: the segment of line to be added to the vector of lines with right direction
+	* \return 
 	*/
 	void lineDirection(std::vector<float> lineAngles, std::vector<int> &bline);
 
@@ -125,9 +140,11 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 03/06/2016
 	* 
-	* \brief given a set of lines detected by the hough transform, calculate best line (average line) points
-	* \param  an ROI of an opencv image, vector of hough lines
-	* \return coordinates of start and end points of average line and integer (0 echec, 1 success)
+	* \brief  given a set of lines detected by the Hough transform, calculate the more representative line (average line)
+	* \param  	roi		: vector of ROIs where rope was detected
+	* 			lines	: vector of detected lines
+	* 			bline	: vector containing the coordinats of more representative line 
+	* \return
 	*/
 	void bestLine(cv::Mat imgBW, cv::Rect roi, std::vector<cv::Vec4i> lines, std::vector<int> &bline);
 
@@ -136,9 +153,13 @@ namespace ml
 	* author : Matheus Laranjeira
 	* date   : 03/06/2016
 	* 
-	* \brief  detects lines in a given ROI using hough transformation
-	* \param  ROI identifier, source image, drawing image
-	* \return coordinates of start and end points of average line and its angle wrt the image vertical
+	* \brief  	detects lines in a given ROI using hough transformation
+	* \param  	imgBW		: BW image with line detected points
+	* 			roi			: vector with ROIs where line was detected
+	* 			imgViz		: image for visualization of ROIs
+	* 			linePoints	: image coordinates of detected segments of lines
+	* 			angles 		: vector containing angles of detected segments of line wrt vertical
+	* \return bool indicating if coordinates of start and end points of average line and its angle wrt the image vertical axis
 	*/
 	bool lineROI(	cv::Mat imgBW,
 					std::vector<cv::Rect> &roi,
@@ -146,14 +167,17 @@ namespace ml
 					std::vector<std::vector<int> > &linePoints,
 					std::vector<float> &lineAngles );
 
-
 	/*!
 	* author : Matheus Laranjeira
 	* date   : 03/06/2016
 	* 
-	* \brief  retrieve the rope pixel coordinates
-	* \param  an opencv image source
-	* \return processed images for visualizations and pixel coordinates of detected rope
+	* \brief 	retrieve the rope pixel coordinates
+	* \param  	imgSrc		: source image
+	* 			imgMap		: BW with detected rope
+	* 			imgViz		: image for visualization of ROIs and detected segments of lines corresponding to the rope
+	* 			locations	: rope pixel coordinates
+	* 			
+	* \return
 	*/
 	void ropePixelCoordinates(cv::Mat imgSrc, cv::Mat &imgMap, cv::Mat &imgViz, std::vector<cv::Point> &locations);
 
@@ -163,7 +187,7 @@ namespace ml
 	* date   : 10/06/2016
 	* 
 	* \brief  generates a vector of ROI bounding the rope from its detected points
-	* \param  an vectors containg some detected rope points
+	* \param  an vectors containg the coordinates of rope segments
 	* \return a vector of ROIs bounding the rope
 	*/
 	void ropeROI(std::vector<std::vector<int> > linePoints, std::vector<cv::Rect> &roi_vec);
